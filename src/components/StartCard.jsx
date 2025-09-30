e11bf82d528356e27caa7b4466895043dd6e4e0d
Gehad127 <gehadadel6111@gmail.com>
2025-09-30 18:14:13 +0300

    Update StartCard.jsx

--- src/components/StartCard.jsx
+++ src/components/StartCard.jsx
@@ -6,60 +6,63 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import AnimatedSection from './AnimatedSection';
 
 const StartCard = ({ title, description, language, image, bottom, backgroundColor, textAlign, color, polygonClipPath, fontSize, height, instructorBackground }) => {
     const theme = useTheme();
     return (
-        <Box sx={{ width: "100%", position: "relative", bottom: { xs: "0", md: bottom }, cursor: "pointer", minWidth: "250px", maxWidth: "250px", }} className='card'>
-            <Card sx={{ height: height || "400px", boxShadow: "none", border: "none", backgroundColor: instructorBackground }}>
-                <div style={{
-                    background: backgroundColor,
-                    clipPath: polygonClipPath,
-                    WebkitClipPath: polygonClipPath,
-                    height: height ? "350px" : "250px",
-                    position: "relative",
-                    overflow: "hidden",
-                    borderRadius: "10px"
-                }}>
-                    <CardMedia
-                        component="img"
-                        loading='lazy'
-                        image={image}
-                        alt={title}
-                        sx={{
-                            objectFit: "fit",
-                            width: "100%",
-                            height: "100%",
-                            position: "absolute",
-                            bottom: 0,
-                        }}
-                    />
-                </div>
-                <CardContent sx={{ textAlign: textAlign, padding: "20px" }}>
-                    <Typography
-                        gutterBottom
-                        variant="h6"
-                        component="div"
-                        sx={{
-                            fontWeight: "600",
-                            fontSize: "17px",
-                            fontFamily: "Quicksand"
-                        }}>
-                        {title}
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        sx={{
-                            fontSize: fontSize || "12px",
-                            fontWeight: "600",
-                            fontFamily: "Quicksand",
-                            color: theme.palette.text.secondary
-                        }}>
-                        <span style={{ color: color }}>{language}</span>{" "}{description}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </ Box>
+        <AnimatedSection animationClass="fadeInUp" delay="0.2s" >
+            <Box sx={{ width: "100%", position: "relative", bottom: { xs: "0", md: bottom }, cursor: "pointer", minWidth: "250px", maxWidth: "250px", }} className='card'>
+                <Card sx={{ height: height || "400px", boxShadow: "none", border: "none", backgroundColor: instructorBackground }}>
+                    <div style={{
+                        background: backgroundColor,
+                        clipPath: polygonClipPath,
+                        WebkitClipPath: polygonClipPath,
+                        height: height ? "350px" : "250px",
+                        position: "relative",
+                        overflow: "hidden",
+                        borderRadius: "10px"
+                    }}>
+                        <CardMedia
+                            component="img"
+                            loading='lazy'
+                            image={image}
+                            alt={title}
+                            sx={{
+                                objectFit: "fit",
+                                width: "100%",
+                                height: "100%",
+                                position: "absolute",
+                                bottom: 0,
+                            }}
+                        />
+                    </div>
+                    <CardContent sx={{ textAlign: textAlign, padding: "20px" }}>
+                        <Typography
+                            gutterBottom
+                            variant="h6"
+                            component="div"
+                            sx={{
+                                fontWeight: "600",
+                                fontSize: "17px",
+                                fontFamily: "Quicksand"
+                            }}>
+                            {title}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            sx={{
+                                fontSize: fontSize || "12px",
+                                fontWeight: "600",
+                                fontFamily: "Quicksand",
+                                color: theme.palette.text.secondary
+                            }}>
+                            <span style={{ color: color }}>{language}</span>{" "}{description}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </ Box>
+        </AnimatedSection>
     );
 };
 
